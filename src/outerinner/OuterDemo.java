7d8bda1b540eab87d8ddb518304f936f0196d1b8
asif <asiftanvir.ny@gmail.com>
2018-09-01 22:12:57 -0400

    my second commit

--- src/outerinner/OuterDemo.java
+++ src/outerinner/OuterDemo.java
@@ -9,6 +9,11 @@ public class OuterDemo {
             System.out.println("Let me show the Cricinfo");
         }
     }
+    public static class InnerDemo2{
+        public void print(){
+            System.out.println("Please print the page for me");
+        }
+    }
 
     public static void main(String[] args) {
         OuterDemo.InnerDemo id = new OuterDemo.InnerDemo();
