472ea53e90a953440f329f237280a713459cf71f
sshahrukhm <sshahrukhm@outlook.com>
2018-09-03 17:38:26 -0400

    thirdcommit

---
+++ src/outerinner/OuterDemo3.java
@@ -0,0 +1,25 @@
+package outerinner;
+
+public class OuterDemo1 {
+	int num;
+
+    public static class InnerDemo{
+        public void showmessage(){
+            System.out.println("Show me the live score");
+        }
+    }
+    public static class InnerDemo2{
+        public void print(){
+            System.out.println("Show me the statics");
+        }
+    }
+
+    public static void main(String[] args) {
+        OuterDemo.InnerDemo id = new OuterDemo.InnerDemo();
+        id.showmessage();
+    }
+
+}
+
+//farzad
+//shahrukh
